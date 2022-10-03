d666abcc0f9d112d48035a4be7d592757ad42c16
SergiSSS <sergiy.sannicandro@outlook.it>
2022-10-03 17:22:36 +0200

    sss

--- js/sanni.js
+++ js/sanni.js
@@ -17,8 +17,18 @@ for (i = 0; i < acc.length; i++) {
 function openNav() {
   document.getElementById("mySidenav").style.width = "200px";
   document.getElementById("mySidenav").style.padding = '5px'
-}
+  console.log(screen.width)
+  if(
+    screen.width < 650
+  ){
+  document.getElementById("mySidenav").style.width = "auto";
+  document.getElementById("mySidenav").style.padding = '0px'
+}}
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mySidenav").style.padding = '0px'
-}
+  if(
+    screen.width < 650
+  ){
+  document.getElementById("mySidenav").style.display = "none"
+}}
