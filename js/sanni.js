64ac8f2d5c469ddf3c0d3ca702cac305be899cae
SergiSSS <sergiy.sannicandro@outlook.it>
2022-10-07 10:25:17 +0200

    m

--- js/sanni.js
+++ js/sanni.js
@@ -1,34 +1,25 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-     // $('.panel').animate({height:'0px'})
-    } else {
-      panel.style.display = "block";
-     // $('.panel').animate({height:'100px'})
-    }
-  });
-} 
+onresize = (event) => {
+  if ( screen.width < 650) {
+    document.getElementById("mySidenav").style.width = "auto";
+    document.getElementById("mySidenav").style.padding = '0px'
+  }
+};
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "200px";
   document.getElementById("mySidenav").style.padding = '5px'
   console.log(screen.width)
-  if(
-    screen.width < 650
-  ){
-  document.getElementById("mySidenav").style.width = "auto";
-  document.getElementById("mySidenav").style.padding = '0px'
-}}
+  if (screen.width < 650) {
+    document.getElementById("mySidenav").style.width = "auto";
+    document.getElementById("mySidenav").style.padding = '0px'
+  }
+}
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("mySidenav").style.padding = '0px'
-  if(
-    screen.width < 650
-  ){
-  document.getElementById("mySidenav").style.display = "none"
-}}
+  if (screen.width < 650) {
+    document.getElementById("mySidenav").style.display = "none"
+  }
+}
